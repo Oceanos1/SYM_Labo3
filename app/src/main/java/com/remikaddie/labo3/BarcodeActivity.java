f66c0414db732e384da1494490a56ce618b6ae33
Edward Ransome <edward.ransome@heig-vd.ch>
2017-12-15 09:37:45 +0100

    Added barcode

--- app/src/main/java/com/remikaddie/labo3/BarcodeActivity.java
+++ app/src/main/java/com/remikaddie/labo3/BarcodeActivity.java
@@ -1,13 +1,38 @@
 package com.remikaddie.labo3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class BarcodeActivity extends Activity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode);
+        Button button = (Button) findViewById(R.id.barCodeButton);
+        textView = (TextView) findViewById(R.id.barcodeResult);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(BarcodeActivity.this);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            textView.setText(scanResult.getContents());
+        }
     }
 }
